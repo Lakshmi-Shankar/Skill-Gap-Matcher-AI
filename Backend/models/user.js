fb12de736cb5e1b131d1de8732a1776ad6ae83ce
Shankar <lakshmi7708671565@gmail.com>
2025-10-30 23:51:19 +0530

    Add a new schema feature and gave it a route

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -15,6 +15,9 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true 
     },
+    skills: {
+        type: [String]
+    },
     createdAt: { 
         type: Date, 
         default: Date.now 
