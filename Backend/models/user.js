1e14a1eb56bcf80bc6493bac05d4e8f785e309e4
Shankar <lakshmi7708671565@gmail.com>
2025-11-02 11:34:01 +0530

    Completed a simple frontend structure and enhanced backend logic

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    password: { 
-        type: String, 
-        required: true 
-    },
-    skills: {
-        type: [String]
-    },
-    createdAt: { 
-        type: Date, 
-        default: Date.now 
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  skills: [
+    {
+      name: { type: String, required: true },
+      level: { type: String, enum: ['Beginner', 'Intermediate', 'Advanced'], default: 'Beginner' }
     }
+  ],
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
