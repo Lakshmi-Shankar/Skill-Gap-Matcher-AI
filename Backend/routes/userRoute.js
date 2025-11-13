edb7fab380d0bbbdd7c7c6e6c53c0477c78f29f7
Shankar <lakshmi7708671565@gmail.com>
2025-11-13 19:58:41 +0530

    Solved cookie issue

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -50,7 +50,7 @@ router.post("/login", async (req, res) => {
         res.cookie('token', token, { 
             httpOnly: true, 
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Lax',
+            sameSite: 'None',
             maxAge: 7*24*60*60*1000 
         });
         res.status(200).json({ message: 'Login successful', user });
