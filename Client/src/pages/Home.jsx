e3e995a2c387da42dd17af719923490205d6ec7a
Shankar <lakshmi7708671565@gmail.com>
2025-11-18 16:35:31 +0530

    Completed the loader

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
   };
 
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex bg-gray-50">
 
       {/* Auto Loader Display */}
       {showLoader && (
